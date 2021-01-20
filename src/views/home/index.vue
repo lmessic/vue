753b810d1559b73d2bb2b7930c4eeb2fc3f66722
li_shaoran <1296601488@qq.com>
2021-01-20 16:49:57 +0800

    ls-1 页面

---
+++ src/views/home/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="250px">
+        <nav-menu />
+      </el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+<script>
+import navMenu from '@/components/Aside'
+export default {
+  components: {
+    navMenu
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
